bc4b93b41dec9be15c6d247c66c915d877983674
vishalpokuri <vishal.pokuri@gmail.com>
2024-08-19 01:13:40 +0530

    Homepage tuning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
 import bg from "./images/bluegrid.png";
+
 function App() {
   return (
     <>
-      <div className="w-screen h-10 bg-black"></div>
-      <div className="w-screen h-screen ">
-        <img src={bg} className="object-fill" alt="bg"></img>
+      <div className="w-screen h-20 bg-black"></div>
+      <div
+        className="w-screen h-screen bg-black bg-cover bg-center"
+        style={{ backgroundImage: `url(${bg})` }}
+      >
+        <div className="flex flex-col items-center text-white pt-32 ">
+          <h1 className="text-6xl leading-tight">TAM-VIT</h1>
+          <p className="font-thin text-5xl leading-tight">welcomes you to</p>
+          <h2 className="text-7xl leading-tight">graVITas'24</h2>
+          <button className="bg-gradient-to-r from-start to-end font-extralight p-2 rounded-md w-36 mt-28">
+            Discover events
+          </button>
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        start: "#4872A7",
+        end: "#00214A",
+      },
+    },
   },
   plugins: [],
 };

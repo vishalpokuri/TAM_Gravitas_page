cc72ff2fc29a71fb574e3ea2837c7ef5ae8d6b9b
vishalpokuri <vishal.pokuri@gmail.com>
2024-08-19 00:04:31 +0530

    Background image work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import bg from "./images/bluegrid.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="w-screen h-10 bg-black"></div>
+      <div className="w-screen h-screen ">
+        <img src={bg} className="object-fill" alt="bg"></img>
+      </div>
+    </>
   );
 }
 
